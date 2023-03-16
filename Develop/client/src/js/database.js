06306c63242c85dac3fff8873c00838e25a5f61a
brachtco <brachtdanny@gmail.com>
2023-03-16 14:16:42 -0600

    Code database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,13 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) =>  {
+  console.error('putDb not implemented')
     const jateDB = await openDB('jate', 1);
     const tx = jateDB.transaction('jate', 'readWrite');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-    
+    const store = tx.objectStore('jate');
+    const request = await store.put({ id: 1, content: content });
+    const result = await request;
+    console.log('data saved to the databae', result);
+};
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+    const jateDB = await openDB('jate', 1);
+    const tx = jateDB.transaction('jate', readOnly);
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+};   
 
 initdb();
