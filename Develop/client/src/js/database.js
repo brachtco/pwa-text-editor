103cc9c6f70b0240728606c26928bfc539502cb3
Danny Bracht <brachtdanny@gmail.com>
2023-03-30 10:34:39 -0600

    Update database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) =>  {
   console.error('putDb not implemented')
-    const jateDB = await openDB('jate', 1);
+    const jateDb = await openDB('jate', 1);
     const tx = jateDB.transaction('jate', 'readWrite');
     const store = tx.objectStore('jate');
     const request = await store.put({ id: 1, content: content });
@@ -25,7 +25,7 @@ export const putDb = async (content) =>  {
 // Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented');
-    const jateDB = await openDB('jate', 1);
+    const jateDb = await openDB('jate', 1);
     const tx = jateDB.transaction('jate', readOnly);
     const store = tx.objectStore('jate');
     const request = store.getAll();
